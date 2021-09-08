dc5c121b8832923bdd36f98c4330b57973e87076
kinght9zz <80620516+kinght9zz@users.noreply.github.com>
2021-09-08 09:19:53 +0800

    Add files via upload

---
+++ js01.js
@@ -0,0 +1,9 @@
+var btn_cle = document.getElementById("btn_cle");
+btn_cle.onclick = function () {
+  alert("清空成功!");
+};
+
+var btn_sub = document.getElementById("btn_sub");
+btn_sub.onclick = function () {
+  alert("提交失败，作者太弱没搞完");
+};
